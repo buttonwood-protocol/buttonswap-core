e7ab2e41bb40c979c32cb966fd0ddf1a705af748
Fiddlekins <fiddlekins@gmail.com>
2023-03-26 19:21:16 +0100

    Added natspec to ButtonswapPair contracts. For entities that might not exist after some design changes the natspec is left as "TODO".

--- src/interfaces/IButtonswapPair/IButtonswapPairErrors.sol
+++ src/interfaces/IButtonswapPair/IButtonswapPairErrors.sol
@@ -4,60 +4,98 @@ pragma solidity ^0.8.13;
 import {IButtonswapERC20Errors} from "../IButtonswapERC20/IButtonswapERC20Errors.sol";
 
 interface IButtonswapPairErrors is IButtonswapERC20Errors {
-    /// @notice Re-entrancy guard prevented method call
+    /**
+     * @notice Re-entrancy guard prevented method call
+     */
     error Locked();
 
-    /// @notice Token transfer failed
+    /**
+     * @notice Token transfer failed
+     */
     error TransferFailed();
 
-    /// @notice User does not have permission for the attempted operation
+    /**
+     * @notice User does not have permission for the attempted operation
+     */
     error Forbidden();
 
-    /// @notice Integer maximums exceeded
+    /**
+     * @notice Integer maximums exceeded
+     */
     error Overflow();
 
-    /// @notice At least one reservoir should always be empty
+    /**
+     * @notice At least one reservoir should always be empty
+     */
     error ReservoirInvariant();
 
-    /// @notice Initial deposit not yet made
+    /**
+     * @notice Initial deposit not yet made
+     */
     error Uninitialized();
 
-    /// @notice The internal balances don't match the actual balance for token0
+    /**
+     * @notice The internal balances don't match the actual balance for token0
+     */
     error Token0BalanceMismatch();
 
-    /// @notice The internal balances don't match the actual balance for token1
+    /**
+     * @notice The internal balances don't match the actual balance for token1
+     */
     error Token1BalanceMismatch();
 
-    /// @notice Mint was attempted with mismatched value on each side
+    /**
+     * @notice Mint was attempted with mismatched value on each side
+     */
     error UnequalMint();
 
-    /// @notice Can't reservoir mint liquidity with both tokens
+    /**
+     * @notice Can't reservoir mint liquidity with both tokens
+     */
     error TwoSidedReservoirMint();
 
-    /// @notice There was not enough liquidity in the reservoir
+    /**
+     * @notice There was not enough liquidity in the reservoir
+     */
     error InsufficientReservoir();
 
-    /// @notice Not enough liquidity was created during mint
+    /**
+     * @notice Not enough liquidity was created during mint
+     */
     error InsufficientLiquidityMinted();
 
-    /// @notice Not enough funds added to mint new liquidity
+    /**
+     * @notice Not enough funds added to mint new liquidity
+     */
     error InsufficientLiquidityAdded();
 
-    /// @notice More liquidity must be burned to be redeemed for non-zero amounts
+    /**
+     * @notice More liquidity must be burned to be redeemed for non-zero amounts
+     */
     error InsufficientLiquidityBurned();
 
-    /// @notice Swap was attempted with zero input
+    /**
+     * @notice Swap was attempted with zero input
+     */
     error InsufficientInputAmount();
 
-    /// @notice Swap was attempted with zero output
+    /**
+     * @notice Swap was attempted with zero output
+     */
     error InsufficientOutputAmount();
 
-    /// @notice Pool doesn't have the liquidity to service the swap
+    /**
+     * @notice Pool doesn't have the liquidity to service the swap
+     */
     error InsufficientLiquidity();
 
-    /// @notice The specified "to" address is invalid
+    /**
+     * @notice The specified "to" address is invalid
+     */
     error InvalidRecipient();
 
-    /// @notice The product of pool balances must not change during a swap (save for accounting for fees)
+    /**
+     * @notice The product of pool balances must not change during a swap (save for accounting for fees)
+     */
     error KInvariant();
 }
