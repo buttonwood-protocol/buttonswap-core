dedc379feb6566032032019c3db8fc99ef968802
SocksNFlops <socks&flops@prl.one>
2023-05-04 16:25:01 -0700

    fix(buttonswap-erc): what the hell was the last thing i did

--- src/ButtonswapERC20.sol
+++ src/ButtonswapERC20.sol
@@ -141,7 +141,8 @@ contract ButtonswapERC20 is IButtonswapERC20 {
     function transferFrom(address from, address to, uint256 value) external returns (bool success) {
         uint256 allowanceFromSender = allowance[from][msg.sender];
         if (allowanceFromSender != type(uint256).max) {
-            emit Approval(from, msg.sender, allowance[from][msg.sender] = allowanceFromSender - value);
+            allowance[from][msg.sender] = allowanceFromSender - value;
+            emit Approval(from, msg.sender, allowanceFromSender - value);
         }
         _transfer(from, to, value);
         success = true;
