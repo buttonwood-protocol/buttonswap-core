a3d800e4f719abc17fd6186db80b196d85f24c1c
Fiddlekins <fiddlekins@gmail.com>
2023-05-16 02:54:17 +0100

    Made event parameter names more consistent

--- src/interfaces/IButtonswapPair/IButtonswapPairEvents.sol
+++ src/interfaces/IButtonswapPair/IButtonswapPairEvents.sol
@@ -7,26 +7,26 @@ interface IButtonswapPairEvents is IButtonswapERC20Events {
     /**
      * @notice Emitted when a {IButtonswapPair-mint} is performed.
      * Some `token0` and `token1` are deposited in exchange for liquidity tokens representing a claim on them.
-     * @param sender The account that supplied the tokens for the mint
+     * @param from The account that supplied the tokens for the mint
      * @param amount0 The amount of `token0` that was deposited
      * @param amount1 The amount of `token1` that was deposited
      * @param to The account that received the tokens from the mint
      */
-    event Mint(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Mint(address indexed from, uint256 amount0, uint256 amount1, address indexed to);
 
     /**
      * @notice Emitted when a {IButtonswapPair-burn} is performed.
      * Liquidity tokens are redeemed for underlying `token0` and `token1`.
-     * @param sender The account that supplied the tokens for the burn
+     * @param from The account that supplied the tokens for the burn
      * @param amount0 The amount of `token0` that was received
      * @param amount1 The amount of `token1` that was received
      * @param to The account that received the tokens from the burn
      */
-    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Burn(address indexed from, uint256 amount0, uint256 amount1, address indexed to);
 
     /**
      * @notice Emitted when a {IButtonswapPair-swap} is performed.
-     * @param sender The account that supplied the tokens for the swap
+     * @param from The account that supplied the tokens for the swap
      * @param amount0In The amount of `token0` that went into the swap
      * @param amount1In The amount of `token1` that went into the swap
      * @param amount0Out The amount of `token0` that came out of the swap
@@ -34,7 +34,7 @@ interface IButtonswapPairEvents is IButtonswapERC20Events {
      * @param to The account that received the tokens from the swap
      */
     event Swap(
-        address indexed sender,
+        address indexed from,
         uint256 amount0In,
         uint256 amount1In,
         uint256 amount0Out,
