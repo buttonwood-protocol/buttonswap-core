7b6fdf7d640a42915efa694585e645e77ad1fee7
Fiddlekins <fiddlekins@gmail.com>
2024-02-17 22:02:58 +0800

    - made committed deploy script args generic

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,12 +6,12 @@ import {ButtonswapFactory} from "../src/ButtonswapFactory.sol";
 
 contract Deploy is BaseScript {
     function run() public virtual broadcast returns (ButtonswapFactory buttonswapFactory) {
-        address _feeToSetter = 0xb1Cc73B1610863D51B5b8269b9162237e87679c3;
-        address _isCreationRestrictedSetter = 0xb1Cc73B1610863D51B5b8269b9162237e87679c3;
-        address _isPausedSetter = 0xb1Cc73B1610863D51B5b8269b9162237e87679c3;
-        address _paramSetter = 0xb1Cc73B1610863D51B5b8269b9162237e87679c3;
-        string memory _tokenName = "Mission LP Token V1";
-        string memory _tokenSymbol = "MSSN-V1";
+        address _feeToSetter = 0x0000000000000000000000000000000000000000;
+        address _isCreationRestrictedSetter = 0x0000000000000000000000000000000000000000;
+        address _isPausedSetter = 0x0000000000000000000000000000000000000000;
+        address _paramSetter = 0x0000000000000000000000000000000000000000;
+        string memory _tokenName = "Buttonswap LP Token V1";
+        string memory _tokenSymbol = "BSWP-V1";
         buttonswapFactory = new ButtonswapFactory(
             _feeToSetter, _isCreationRestrictedSetter, _isPausedSetter, _paramSetter, _tokenName, _tokenSymbol
         );
